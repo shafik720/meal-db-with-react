544f61d8c31c8eab2a872fb0fdfb13aa6bdc1c33
shafik720 <shafikrasel5@gmail.com>
2022-10-26 18:31:25 +0600

    dfgdfg

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,8 +4,11 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header-div">
+            <div className="left-div">
+                
+            </div>
+            <div className="right-div"></div>
         </div>
     );    
 };
