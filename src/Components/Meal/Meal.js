c6df260ad6a2bd1c1e96e579a97faf78ed7b87e6
shafik720 <shafikrasel5@gmail.com>
2022-10-26 21:53:00 +0600

    dfgdfg

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Meal.css'
 
 const Meal = (props) => {
-    console.log(props)
+    console.log(props);
+    const{strMealThumb, strMeal, strArea, strCategory} = props.index
     return (
         <div>
-            <h2>Hello</h2>
+            <div className="meal-card">
+                <div className="meal-top">
+                    <img src={strMealThumb} alt=""/>
+                </div>
+            </div>
         </div>
     );
 };
