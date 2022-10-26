dc2b79db77f98a217672b5eb84d8f06ba5967214
shafik720 <shafikrasel5@gmail.com>
2022-10-26 23:58:50 +0600

    dfgdfg

---
+++ src/Components/CartCard/CartCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CartCard.css';
+
+const CartCard = () => {
+    return (
+        <div>
+            <h2>Hello From Cart Card</h2>
+        </div>
+    );
+};
+
+export default CartCard;
