a2e4ceeb46cff952861a9fe8326f6da7aa6abd48
shafik720 <shafikrasel5@gmail.com>
2022-10-26 19:54:26 +0600

    dfgdfg

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import Foods from '../Foods/Foods';
+
 import './Body.css'
 
 const Body = () => {
-    const[foods, setFoods] = useState([]);
-    useEffect(()=>{
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
-        .then(res=>res.json())
-        .then(data=>setFoods(data.meals))
-    },[])
+    // const[foods, setFoods] = useState([]);
+    // useEffect(()=>{
+    //     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
+    //     .then(res=>res.json())
+    //     .then(data=>setFoods(data.meals))
+    // },[])    
 
+    // let {food, setFood} = useState({});
+    const[food, setFood] = useState([]);
+
+    let x = '';
+    function searchFunction(){
+        let searchField = document.getElementById('searchText');
+        x = searchField.value;    
+        setFood(x);
+    }
     return (
         <div className="mainDiv">
-            <div className="left-div">
-                <h2>Total Food Found : {foods.length} </h2>            
+            <div className="left-div"> 
+                <div className="search-div">
+                    <input type="text" id="searchText" placeholder="Search Your food Here" />
+                    <button onClick={searchFunction}>Search</button>
+                </div>    
+                <div className="result-div">
+                    <Foods food={food}></Foods>
+                </div>     
             </div>
             <div className="right-div">
                 <h2>Ok</h2>
--- src/Components/Foods/Foods.js
+++ src/Components/Foods/Foods.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Foods.css'
 
 const Foods = (props) => {
+    let {food} = props;
+    console.log(food);
     return (
         <div>
             <h2>Hello Boy</h2>
