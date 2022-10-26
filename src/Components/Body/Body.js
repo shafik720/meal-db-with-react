015d569865b8fe223e2a4b688951493b05ce59cf
shafik720 <shafikrasel5@gmail.com>
2022-10-26 20:44:50 +0600

    dfgdfg

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,33 +3,19 @@ import Foods from '../Foods/Foods';
 
 import './Body.css'
 
-const Body = () => {
-    // const[foods, setFoods] = useState([]);
-    // useEffect(()=>{
-    //     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
-    //     .then(res=>res.json())
-    //     .then(data=>setFoods(data.meals))
-    // },[])    
-
-    // let {food, setFood} = useState({});
-    const[food, setFood] = useState([]);
-
-    let x = '';
-    function searchFunction(){
-        let searchField = document.getElementById('searchText');
-        x = searchField.value;    
-        setFood(x);
-    }
+const Body = () => {  
+    
+    const[meal, setMeal] = useState([]);
+    useEffect(()=>{
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
+        .then(res=>res.json())
+        .then(data=>setMeal(data));
+    },[])
+    
     return (
         <div className="mainDiv">
             <div className="left-div"> 
-                <div className="search-div">
-                    <input type="text" id="searchText" placeholder="Search Your food Here" />
-                    <button onClick={searchFunction}>Search</button>
-                </div>    
-                <div className="result-div">
-                    <Foods food={food}></Foods>
-                </div>     
+                 
             </div>
             <div className="right-div">
                 <h2>Ok</h2>
--- src/Components/Foods/Foods.js
+++ src/Components/Foods/Foods.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Foods.css'
 
-const Foods = (props) => {
-    let {food} = props;
+const Foods = (props) => {   
     
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
-        .then(res=>res.json())
-        .then(data=>console.log(data))
     return (
         <div>
-            <h2>Hello Boy</h2>
+            
         </div>
     );
 };
