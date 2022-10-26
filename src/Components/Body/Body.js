60f3cd5eb48ab4a8eac242f7b2c5eb25f88147ef
shafik720 <shafikrasel5@gmail.com>
2022-10-26 18:55:19 +0600

    dfgdfg

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Body.css'
 
 const Body = () => {
+    const[foods, setFoods] = useState([]);
+    useEffect(()=>{
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
+        .then(res=>res.json())
+        .then(data=>setFoods(data.meals))
+    },[])
+
     return (
         <div>
-            <h2>Hello From Body</h2>
+            <h2>Total Food Found : {foods.length} </h2>
         </div>
     );
 };
