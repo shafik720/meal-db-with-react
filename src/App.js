7dc25a39095162ec232b4ef91e0b3e1886d72e4c
shafik720 <shafikrasel5@gmail.com>
2022-10-26 18:42:42 +0600

    dfgdfg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <div className="header-div">
             <div className="left-div">
-                
+                <h1><span><FontAwesomeIcon icon={faUtensils} /></span> Food Bangla</h1>
             </div>
-            <div className="right-div"></div>
+            <div className="right-div">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Menu</a>
+                <a href="">Contact</a>
+            </div>
         </div>
     );    
 };
