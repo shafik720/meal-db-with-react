010aee28a7c30cb193f6c546d58942ce3483dc96
shafik720 <shafikrasel5@gmail.com>
2022-10-26 23:03:13 +0600

    dfgdfg

--- src/Components/Utilities/storage.js
+++ src/Components/Utilities/storage.js
@@ -2,10 +2,14 @@
 
 function addToDb(id){
     let mealCart = JSON.parse(localStorage.getItem('meal-cart') || '{}');
-    mealCart[id] = 1;
-    localStorage.setItem('meal-cart', JSON.stringify(mealCart));
-    console.log(mealCart);
     
+    if(id in mealCart){
+        mealCart[id] = mealCart[id] + 1;
+    }else{
+        mealCart[id] = 1;
+    }
+    localStorage.setItem('meal-cart', JSON.stringify(mealCart));
+
 }
 
 
